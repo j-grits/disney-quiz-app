e5f0d58f5611325a09e473dd905afb31c0bc35bc
j-grits <142440972+j-grits@users.noreply.github.com>
2024-02-13 18:16:22 -0500

    Disney Trivia React App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Quiz from "./Quiz";
+import { jsQuiz } from "./constants";
+
+function App() {
+  return <>
+  <Quiz questions={jsQuiz.questions} />
+  </>
+
+  
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,198 @@
+export const jsQuiz = {
+    questions: [
+      {
+        question:
+          "Disney's Animal Kingdom theme park opened in Florida in which year?",
+        choices: [
+          "1996",
+          "1994",
+          "1998",
+          "2000",
+        ],
+        type: "MCQs",
+        correctAnswer: "1998",
+      },
+      {
+        question: "What is the name of the family in Mary Poppins?",
+        choices: [
+          "Stevens",
+          "Cardson",
+          "Smith",
+          "Banks",
+        ],
+        type: "MCQs",
+        correctAnswer: "Banks",
+      },
+      {
+        question:
+          "Disney opened its first European park close to which city in 1992?",
+        choices: ["Berlin", "Paris", "London", "Barcelona"],
+        type: "MCQs",
+        correctAnswer: "Paris",
+      },
+      {
+        question: "In The Princess and the Frog, what is the name of the trumpet-playing alligator?",
+        choices: ["Louis", "Jamie", "Jared", "Tuck"],
+        type: "MCQs",
+        correctAnswer: "Louis",
+      },
+      {
+        question: "Which Disney character is a charismatic and mischievous pirate fox in Zootopia?",
+        choices: [
+          "Walt Strop",
+          "James Tuter",
+          "Brad North",
+          "Nick Wilde",
+        ],
+        type: "MCQs",
+        correctAnswer: "Nick Wilde",
+      },
+      {
+        question:
+          "What is the name of Donald Duck's sister?",
+        choices: [
+          "Della",
+          "Tela",
+          "Bonnie",
+          "Nikki",
+        ],
+        type: "MCQs",
+        correctAnswer: "Della",
+      },
+      {
+        question: "What is the title of the Disney film about a young girl who enters a magical world through a secret door?",
+        choices: [
+          "Caroline",
+          "Chronicles of Narnia",
+          "Coraline",
+          "Finding Nemo",
+        ],
+        type: "MCQs",
+        correctAnswer: "Coraline",
+      },
+      {
+        question:
+          "In Moana, what is the name of the demigod who accompanies Moana on her journey?",
+        choices: ["Tefeti", "Maui", "Pui", "Cluckles"],
+        type: "MCQs",
+        correctAnswer: "Maui",
+      },
+      {
+        question: "In Beauty and the Beast, what song does Belle sing about her provincial life?",
+        choices: ["Gaston", "There Must Be More", "Belle", "Bonjour"],
+        type: "MCQs",
+        correctAnswer: "Belle",
+      },
+      {
+        question: "Which Disney villain has a magic mirror and a desire to be the fairest of them all?",
+        choices: [
+          "Ursula",
+          "The Wicked Witch",
+          "The Evil Queen",
+          "Cruella de Vil",
+        ],
+        type: "MCQs",
+        correctAnswer: "The Evil Queen",
+      },
+      {
+        question:
+          "Who is the wicked stepmother in Cinderella who prevents her from attending the royal ball?",
+        choices: [
+          "Mrs. Facilier",
+          "Lady Tremaine",
+          "Lady Beatrice",
+          "Miss Janice",
+        ],
+        type: "MCQs",
+        correctAnswer: "Lady Tremaine",
+      },
+      {
+        question: "In Hercules, who is the villainous god of the underworld?",
+        choices: [
+          "Zeus",
+          "Hephaestus",
+          "Dionysus",
+          "Hades",
+        ],
+        type: "MCQs",
+        correctAnswer: "Hades",
+      },
+      {
+        question:
+          " What was the first roller-coaster-style attraction in Disney World?",
+        choices: ["Space Mountain", "Matterhorn", "The Barnstormer", "Big Thunder Mountain"],
+        type: "MCQs",
+        correctAnswer: "Matterhorn",
+      },
+      {
+        question: "Which Disney park is known for its elaborate parades and shows, including the Festival of Fantasy Parade?",
+        choices: ["Disneyland Forever", "Disneyland Paris", "Tokyo Disneyland", "Magic Kingdom"],
+        type: "MCQs",
+        correctAnswer: "Disneyland Paris",
+      },
+      {
+        question: "Which Disney movie features a group of singing and dancing penguins?",
+        choices: [
+          "Madagascar",
+          "Mary Poppins",
+          "Happy Feet",
+          "Surf's Up",
+        ],
+        type: "MCQs",
+        correctAnswer: "Mary Poppins",
+      },
+      {
+        question:
+          "In Lady and the Tramp, what type of dog is Lady?",
+        choices: [
+          "Schnauzer",
+          "Cocker Spaniel",
+          "Jack Russel Terrier",
+          "Mutt",
+        ],
+        type: "MCQs",
+        correctAnswer: "Cocker Spaniel",
+      },
+      {
+        question: "What is the name of the adventurous chameleon in the movie Tangled?",
+        choices: [
+          "Pascal",
+          "Trident",
+          "Frankie",
+          "Ravioli",
+        ],
+        type: "MCQs",
+        correctAnswer: "Pascal",
+      },
+      {
+        question:
+          "What is the name of the horse in Tangled who helps Rapunzel and Flynn Rider on their journey?",
+        choices: ["Maximus", "Flynn", "Stev", "Pongo"],
+        type: "MCQs",
+        correctAnswer: "Maximus",
+      },
+      {
+        question: "In which year was the first full-length animated feature film released by Disney?",
+        choices: ["1929", "1936", "1937", "1932"],
+        type: "MCQs",
+        correctAnswer: "1937",
+      },
+      {
+        question: "Who was the original voice of Mickey Mouse?",
+        choices: [
+          "Lorne Michaels",
+          "Mel Blanc",
+          "Walt Disney",
+          "Peter Pan",
+        ],
+        type: "MCQs",
+        correctAnswer: "Walt Disney",
+      },
+    ],
+  };
+
+  export const resultInitialState = {
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  };
